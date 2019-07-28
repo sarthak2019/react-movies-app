746d711a84978a71186ab6f8eb6c62f9da5a3f29
sarthak2019 <sarthakmohapatra2@gmail.com>
2019-07-28 12:47:20 +0530

    Made some changes for merge conflict

--- src/index.js
+++ src/index.js
@@ -1,17 +1,5 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-<<<<<<< HEAD
-import './index.css';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
-
-ReactDOM.render(<App />, document.getElementById('root'));
-
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister();
-=======
 import './index.css';  
 import 'typeface-roboto'; 
 import registerServiceWorker from './registerServiceWorker';
@@ -19,4 +7,3 @@ import Home from './screens/home/Home'
 
 ReactDOM.render(<Home />, document.getElementById('root'));
 registerServiceWorker();
->>>>>>> cd5579c24489409039956c0b64ed3147a97397bc
